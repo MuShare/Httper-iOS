ee7c49c915800a657c38ea9d72804229f7544c16
limeng <lm2343635@126.com>
2016-12-30 02:30:42 +0900

    Search whois and load by UIWebView.

---
+++ Httper/WhoisViewController.swift
@@ -0,0 +1,68 @@
+//
+//  WhoisViewController.swift
+//  Httper
+//
+//  Created by 李大爷的电脑 on 30/12/2016.
+//  Copyright © 2016 limeng. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import Kanna
+
+let baseURL = "https://www.whois.com"
+
+class WhoisViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var domainTextField: UITextField!
+    @IBOutlet weak var resultWebView: UIWebView!
+    @IBOutlet weak var loadingActivityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var searchBarButtonItem: UIBarButtonItem!
+    
+    var css = "<style>"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        do {
+            let cssPath = Bundle.main.path(forResource: "whois", ofType: "css")
+            css += try String(contentsOfFile: cssPath!, encoding: .utf8)
+        } catch let error {
+            print("Failed reading, Error: " + error.localizedDescription)
+        }
+        css += "</style>"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        domainTextField.becomeFirstResponder()
+    }
+    
+    // MARK: - UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == domainTextField {
+            whois(searchBarButtonItem)
+        }
+        return true
+    }
+
+    // MARK: - Action
+    @IBAction func whois(_ sender: Any) {
+        if domainTextField.isFirstResponder {
+            domainTextField.resignFirstResponder()
+        }
+        searchBarButtonItem.isEnabled = false
+        loadingActivityIndicatorView.startAnimating()
+        Alamofire.request(baseURL + "/whois/" + domainTextField.text!).responseString { response in
+            var html = "<meta name='format-detection' content='telephone=no'/>" + self.css
+            if let doc = HTML(html: response.result.value!, encoding: .utf8) {
+                for link in doc.css(".df-block") {
+                    html += link.toHTML!
+                }
+                self.resultWebView.loadHTMLString(html, baseURL: URL.init(string: baseURL))
+                self.resultWebView.isHidden = false
+                self.searchBarButtonItem.isEnabled = true
+                self.loadingActivityIndicatorView.stopAnimating()
+            }
+        }
+    }
+}
--- Podfile
+++ Podfile
@@ -4,5 +4,6 @@ target 'Httper' do
     pod 'Alamofire', '~> 4.2'
     pod 'M80AttributedLabel', '~> 1.6'
     pod 'UILabel+Copyable', '~> 1.0'
+    pod 'Kanna', '~> 2.1.0'
 end
 
