5656e379791c3e5463bbd308f55fd070c2f2e6c3
Meng Li <lm2343635@126.com>
2017-02-01 22:51:04 +0900

    Redesign ProjectViewController.

--- Httper/Request+CoreDataClass.swift
+++ Httper/Request+CoreDataClass.swift
@@ -2,8 +2,8 @@
 //  Request+CoreDataClass.swift
 //  Httper
 //
-//  Created by 李大爷的电脑 on 14/12/2016.
-//  Copyright © 2016 limeng. All rights reserved.
+//  Created by 李大爷的电脑 on 01/02/2017.
+//  Copyright © 2017 limeng. All rights reserved.
 //
 
 import Foundation
