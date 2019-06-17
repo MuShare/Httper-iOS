88f600a9a2bbbacd48d30d8cd1c35e3498e135a8
Meng Li <lm2343635@126.com>
2019-06-18 08:24:24 +0900

    Implement IPAddress cells.

--- Httper/Cell/IPAddressHeadTableViewCell.swift
+++ Httper/Cell/IPAddressHeadTableViewCell.swift
@@ -9,12 +9,63 @@
 import RxDataSourcesSingleSection
 import UIKit
 
+private struct Const {
+    
+    struct icon {
+        static let size = 30
+        static let marginTop = 10
+    }
+    
+    struct name {
+        static let marginVertical = 5
+    }
+    
+}
+
 class IPAddressHeadTableViewCell: UITableViewCell {
+    
+    private lazy var iconImageView = UIImageView()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(iconImageView)
+        addSubview(nameLabel)
+        createConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func createConstraints() {
+        
+        iconImageView.snp.makeConstraints {
+            $0.size.equalTo(Const.icon.size)
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(Const.icon.marginTop)
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(iconImageView.snp.bottom).offset(Const.name.marginVertical)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-Const.name.marginVertical)
+        }
+        
+    }
     
 }
 
 struct IPAddressHead {
-    
+    var icon: UIImage?
+    var name: String
 }
 
 extension IPAddressHeadTableViewCell: Configurable {
@@ -22,7 +73,8 @@ extension IPAddressHeadTableViewCell: Configurable {
     typealias Model = IPAddressHead
     
     func configure(_ model: IPAddressHead) {
-        
+        iconImageView.image = model.icon
+        nameLabel.text = model.name
     }
     
 }
--- Httper/Cell/IPAddressInfoTableViewCell.swift
+++ Httper/Cell/IPAddressInfoTableViewCell.swift
@@ -9,12 +9,57 @@
 import RxDataSourcesSingleSection
 import UIKit
 
+private struct Const {
+    static let marginHorizontal = 17
+    static let marginVertical = 15
+}
+
 class IPAddressInfoTableViewCell: UITableViewCell {
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(nameLabel)
+        addSubview(infoLabel)
+        createConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func createConstraints() {
+        
+        nameLabel.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(Const.marginHorizontal)
+            $0.top.equalToSuperview().offset(Const.marginVertical)
+            $0.bottom.equalToSuperview().offset(-Const.marginVertical)
+        }
+        
+        infoLabel.snp.makeConstraints {
+            $0.right.equalToSuperview().offset(-Const.marginHorizontal)
+            $0.centerY.equalToSuperview()
+        }
+        
+    }
     
 }
 
 struct IPAddressInfo {
-    
+    var name: String
+    var info: String
 }
 
 extension IPAddressInfoTableViewCell: Configurable {
