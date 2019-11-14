d11a976a564c1be20cab79ea3af31bdc254b200a
Meng Li <lm2343635@126.com>
2019-11-14 18:58:48 +0900

    Add KeyboardAccessoryViewController.

---
+++ Httper/Cell/KeyboardAccessoryCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  KeyboardAccessoryCollectionViewCell.swift
+//  Httper
+//
+//  Created by Meng Li on 2019/11/14.
+//  Copyright © 2019 limeng. All rights reserved.
+//
+
+import RxDataSourcesSingleSection
+
+class KeyboardAccessoryCollectionViewCell: UICollectionViewCell {
+
+}
+
+struct KeyboardAccessory {
+
+}
+
+extension KeyboardAccessoryCollectionViewCell: Configurable {
+
+    typealias Model = KeyboardAccessory
+
+    func configure(_ model: KeyboardAccessory) {
+
+    }
+    
+}
---
+++ Httper/Controller/Settings/KeyboardAccessoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  KeyboardAccessoryViewModel.swift
+//  Httper
+//
+//  Created by Meng Li on 2019/11/14.
+//  Copyright © 2019 limeng. All rights reserved.
+//
+
+import RxSwift
+
+class KeyboardAccessoryViewModel: BaseViewModel {
+    
+}
