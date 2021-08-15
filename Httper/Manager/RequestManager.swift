78feb8bd947515f02e254235e6dd4f3f2adc2ee9
Meng Li <meng.li@linecorp.com>
2021-08-15 09:33:47 +0900

    Fix for array parameter

--- Httper/Manager/RequestManager.swift
+++ Httper/Manager/RequestManager.swift
@@ -15,11 +15,25 @@ final class RequestManager {
     static let shared = RequestManager()
 
     func send(_ request: RequestData) -> Observable<(HTTPURLResponse, Data)> {
+        var url = request.url
+        var singleParameters: [String: Any] = [:]
+        var isUrlParameterAppended = false
+        request.parameters.forEach { key, value in
+            if let values = value as? [String] {
+                url += !isUrlParameterAppended ? "?" : "&"
+                url += values.map { "\(key)=\($0)" }
+                    .joined(separator: "&")
+                    .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+                isUrlParameterAppended = true
+            } else {
+                singleParameters[key] = value
+            }
+        }
         return RxAlamofire
             .request(
                 request.httpMethod,
-                request.url,
-                parameters: request.parameters,
+                url,
+                parameters: singleParameters,
                 encoding: request.body.isEmpty ? URLEncoding.default : request.body,
                 headers: HTTPHeaders(request.headers.map { HTTPHeader(name: $0.key, value: $0.value) })
             )
